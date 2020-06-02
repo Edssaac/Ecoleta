747a1c460cc721da5b9b08e60bda49692fb089a7
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2020-06-02 19:52:42 -0300

    Add files via upload

---
+++ scripts/intro.js
@@ -0,0 +1,47 @@
+// document.write("Hello")
+
+// var test
+// test = "hello"
+
+// document.write(test + test)
+// document.write("\n")
+// document.write(test , "Ola")
+
+// const n1 = 23
+
+// document.write(n1%2)
+
+// const pessoa = {
+//   altura: "1.80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// const lista = ["abacate", "melao", "feijao"]
+
+// document.write(lista[2], " não é uma fruta ") 
+
+// function batata(name) {
+  
+//   document.write(name)
+// } 
+
+// batata("wesley")
+// batata("foael")
+// batata("okdsey")
+
+// const Nota = 2
+
+// if(Nota > 5){ 
+//   document.write("Aprovado")}
+// else{
+//   document.write("Reprovado")}
+
+for (i = 0; i < 10; i++){
+    document.write("<p>", i, "</p>")
+  }
